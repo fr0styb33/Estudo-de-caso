db7a86aae16a7f9744e8c07958b6fe89f89003e9
Emily Elisabeth Neves <emily.en@aluno.ifsc.edu.br>
2022-11-21 15:01:22 -0300

    Cardápio modelo
    Uma forma de montar a estrutura dos podutos

--- iTicket/src/controle/cardapio.java
+++ iTicket/src/controle/cardapio.java
@@ -1,5 +1,21 @@
 package controle;
 
-public class cardapio {
+public class cardapio { // método: listagem de produtos
+
+	public static void main (String[]args) {
+		//BEBIDAS
+		Bebidas bebida1= new Bebidas();
+		bebida1.setNome("Energético");
+		bebida1.
+		
+		//ALMOCO
+		
+		
+		//LANCHE
+		
+		
+	}
+
+	ArrayList<Produtos> prod = new ArrayList<>();
 
 }
